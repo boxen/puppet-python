c2ad6622e86d4de373cb8766b3768b04e9afe120
Bryan Veloso <bryan@revyver.com>
2013-01-27 12:04:12 -0800

    Update the python spec.

--- spec/classes/python_spec.rb
+++ spec/classes/python_spec.rb
@@ -8,7 +8,7 @@ describe 'python' do
   end
 
   it do
-    should contain_package('python').with_ensure('latest')
+    should contain_package('python').with_ensure('2.7.3-boxen1')
 
     should contain_file('/opt/boxen/env.d/python.sh').with_source('puppet:///modules/python/python.sh')
   end
