6252fae4e6deaeb85c9317a7fdf2826b4c1810bb
Bryan Veloso <bryan@revyver.com>
2013-01-27 02:46:13 -0800

    New style init.pp.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,14 +7,16 @@ class python {
   require boxen::config
   require xquartz
 
-  package {
-    'python':
-      ensure => latest;
-    ['python-distribute', 'python-pip']:
-      require => Package['python'] ;
+  hombrew::formula { 'python':
+    before => Package['boxen/brews/python']
+  }
+
+  package { 'boxen/brews/python':
+    ensure => '2.7.3'
   }
 
   file { "${boxen::config::envdir}/python.sh":
     source  => 'puppet:///modules/python/python.sh'
   }
+
 }
