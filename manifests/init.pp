2a2cba3a88e397c174dd7f430ecbc97b13632577
Will Farrington <wfarr@github.com>
2013-02-11 16:33:44 -0800

    fix path

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,19 +9,21 @@ class python {
   include homebrew::config
   include xquartz
 
+  $version = '2.7.3-boxen1'
+
   homebrew::formula { 'python':
     before => Package['boxen/brews/python']
   }
 
   package { 'boxen/brews/python':
-    ensure  => '2.7.3-boxen1',
+    ensure  => $version,
     require => Class['xquartz']
   }
 
   file { "${homebrew::config::installdir}/lib/python2.7/site-packages":
     force   => true,
     ensure  => link,
-    target  => "${homebrew::config::installdir}/Cellar/python/2.7.3/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages",
+    target  => "${homebrew::config::installdir}/Cellar/python/${version}/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages",
     require => Package['boxen/brews/python']
   }
 
